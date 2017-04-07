4b562ecbd101261076e598ca5f66f4e3a19e17f7
Dan Windheim <danielwindheim@gmail.com>
2017-04-06 18:55:29 -0700

    Created list of TV and Movies folders to compare to files list

--- filefinder.py
+++ filefinder.py
@@ -1,16 +1,33 @@
 import os, pprint, re, shutil
 
-toMove = []
-
+# Creates list of .mkv, .mp4, and .avi files in New Downloads
+fullPath = []
+onlyFile = []
 for dirPath, dirNames, fileNames in os.walk("/mnt/nd"):
     for fileName in [f for f in fileNames if f.endswith(".mkv") or f.endswith(".avi") or f.endswith(".mp4")]:
-        toMove.append(os.path.join(dirPath, fileName)) 
+        fullPath.append(os.path.join(dirPath, fileName))
+        onlyFile.append(fileName)
 
-# pprint.pprint(toMove)
+# Turns periods in filenames to spaces
+per2Space = []
+for x in onlyFile:
+    j = x.replace('.', ' ')
+    per2Space.append(j)
+    
 
-fileRegex = re.compile(r'The\.Expanse.\w+', re.I)
+# pprint.pprint(onlyFile)
 
-for file in toMove:
-    print(fileRegex.findall(file))
+# REGEX TO FIND FILES 
 
+fileRegex = re.compile(r'The\.Expanse.\w*', re.I)
+
+#for file in onlyFile:
+#    print(fileRegex.findall(file))    
+
+# Creates list of folders in TV and Movies
+tvnmDir = []
+for folder in os.listdir('/mnt/tvnm'):
+    tvnmDir.append(folder)
+
+pprint.pprint(set(tvnmDir) & set(noSpace))
 
