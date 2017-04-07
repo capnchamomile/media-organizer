865db64e414777c9f0bf4556104cd293c3a7a31e
Dan Windheim <danielwindheim@gmail.com>
2017-04-06 18:22:25 -0700

    Moved repo

---
+++ localtest.py
@@ -0,0 +1,16 @@
+import os, pprint, re, shutil
+
+toMove = []
+
+for dirPath, dirNames, fileNames in os.walk("/home/dan/Desktop"):
+    for fileName in [f for f in fileNames if f.endswith(".mkv") or f.endswith(".avi") or f.endswith(".mp4")]:
+        toMove.append(os.path.join(dirPath, fileName)) 
+
+# pprint.pprint(toMove)
+
+fileRegex = re.compile(r'The\.Expanse.\w+', re.I)
+
+for file in toMove:
+    print(fileRegex.findall(file))
+
+
