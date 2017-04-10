6dc6df369101dbc07159bb64a4c55166947d82d3
Dan Windheim <danielwindheim@gmail.com>
2017-04-09 17:29:53 -0700

    Changed global variables to local

--- filefinder.py
+++ filefinder.py
@@ -1,28 +1,29 @@
-import os, pprint, re, shutil
+import os, re, shutil
 
 # Creates list of .mkv, .mp4, and .avi files in New Downloads
 fullPath = []
 onlyFile = []
-for dirPath, dirNames, fileNames in os.walk("/mnt/nd"):
+for dirPath, subDirs, fileNames in os.walk("/mnt/nd"):
     for fileName in [f for f in fileNames if f.endswith(".mkv") or f.endswith(".avi") or f.endswith(".mp4")]:
         fullPath.append(os.path.join(dirPath, fileName))
         onlyFile.append(fileName)
 
 # Turns periods in filenames to spaces
-per2Space = []
+
 for x in fullPath:
+    per2Space = []
     j = x.replace('.', ' ')
     per2Space.append(j)
 
-
-
 # Creates list of folders in TV and Movies
-tvnmDir = []
+
 for folder in os.listdir('/mnt/tvnm'):
+    tvnmDir = []
     tvnmDir.append(folder)
 
 
-    
+
+#This isn't going to work    
 '''
 # Strips extraneous characters for comparison 
 set1 = []
